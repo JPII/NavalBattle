20a5c44e594300aceb1c8a892caaf1aaf77c0608
abauer <abauer@jpiicardinals.org>
2012-08-22 12:33:28 -0500

    Added frame for grid
    fixes issue #5
    Info came from GridWorld

---
+++ src/com/jpii/navalbattle/game/Grid.java
@@ -0,0 +1,95 @@
+package com.jpii.navalbattle.game;
+
+import java.util.ArrayList;
+
+public class Grid<E>
+{
+    private Object[][] occupantArray; // the array storing the grid elements
+
+    /**
+     * Constructs an empty bounded grid with the given dimensions.
+     * (Precondition: <code>rows > 0</code> and <code>cols > 0</code>.)
+     * @param rows number of rows in BoundedGrid
+     * @param cols number of columns in BoundedGrid
+     */
+    public Grid(int rows, int cols)
+    {
+        if (rows <= 0)
+            throw new IllegalArgumentException("rows <= 0");
+        if (cols <= 0)
+            throw new IllegalArgumentException("cols <= 0");
+        occupantArray = new Object[rows][cols];
+    }
+
+    public int getNumRows()
+    {
+        return occupantArray.length;
+    }
+
+    public int getNumCols()
+    {
+        // Note: according to the constructor precondition, numRows() > 0, so
+        // theGrid[0] is non-null.
+        return occupantArray[0].length;
+    }
+
+    public boolean isValid(Location loc)
+    {
+        return 0 <= loc.getRow() && loc.getRow() < getNumRows()
+                && 0 <= loc.getCol() && loc.getCol() < getNumCols();
+    }
+
+    public ArrayList<Location> getOccupiedLocations()
+    {
+        ArrayList<Location> theLocations = new ArrayList<Location>();
+
+        // Look at all grid locations.
+        for (int r = 0; r < getNumRows(); r++)
+        {
+            for (int c = 0; c < getNumCols(); c++)
+            {
+                // If there's an object at this location, put it in the array.
+                Location loc = new Location(r, c);
+                if (get(loc) != null)
+                    theLocations.add(loc);
+            }
+        }
+
+        return theLocations;
+    }
+
+    @SuppressWarnings("unchecked")
+	public E get(Location loc)
+    {
+        if (!isValid(loc))
+            throw new IllegalArgumentException("Location " + loc
+                    + " is not valid");
+        return (E) occupantArray[loc.getRow()][loc.getCol()];
+    }
+
+    public E put(Location loc, E obj)
+    {
+        if (!isValid(loc))
+            throw new IllegalArgumentException("Location " + loc
+                    + " is not valid");
+        if (obj == null)
+            throw new NullPointerException("obj == null");
+
+        // Add the object to the grid.
+        E oldOccupant = get(loc);
+        occupantArray[loc.getRow()][loc.getCol()] = obj;
+        return oldOccupant;
+    }
+
+    public E remove(Location loc)
+    {
+        if (!isValid(loc))
+            throw new IllegalArgumentException("Location " + loc
+                    + " is not valid");
+        
+        // Remove the object from the grid.
+        E r = get(loc);
+        occupantArray[loc.getRow()][loc.getCol()] = null;
+        return r;
+    }
+}
