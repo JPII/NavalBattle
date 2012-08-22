2c47ee508239d2db570d79dbbea9ef03eb906bbc
abauer <abauer@jpiicardinals.org>
2012-08-21 19:30:57 -0500

    Personal Update / Warning fix
    NavalBattle packaged
    RoketGamer constructor fixed

---
+++ src/com/jpii/navalbattle/NavalBattle.java
@@ -0,0 +1,5 @@
+package com.jpii.navalbattle;
+
+public class NavalBattle {
+	
+}
--- src/com/jpii/roketgamer/RoketGamer.java
+++ src/com/jpii/roketgamer/RoketGamer.java
@@ -5,7 +5,7 @@ public class RoketGamer {
 	/**
 	 * Default constructor for <code>RoketGamer</code>
 	 */
-	public void RoketGamer() {
+	public RoketGamer() {
 		
 	}
 }
