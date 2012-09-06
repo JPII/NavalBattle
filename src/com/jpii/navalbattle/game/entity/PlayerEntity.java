78db7393c12f1448fb860fc69e145b433f8ff818
TexasGamer <tpgaubert@gmail.com>
2012-09-06 10:32:19 -0500

    Added PlayerEntity (needs work)

---
+++ src/com/jpii/navalbattle/game/entity/PlayerEntity.java
@@ -0,0 +1,30 @@
+package com.jpii.navalbattle.game.entity;
+
+import java.awt.Image;
+
+import com.jpii.navalbattle.game.Location;
+
+public class PlayerEntity extends MortalEntity {
+
+	/**
+	 * Construct <code>PlayerEntity</code>.
+	 * @param location
+	 * @param image
+	 * @param health
+	 */
+	public PlayerEntity(Location location, Image image, int health) {
+		super(location, image, health);
+	}
+	
+	/**
+	 * Called when attacked
+	 * @param damage
+	 */
+	public void onAttacked(int damage, Entity attacker) {
+		damage(damage);
+
+		if(getHealth() <= 0) {
+			setActive(false);
+		}
+	}
+}
