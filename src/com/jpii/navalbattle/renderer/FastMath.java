aacd2f75e902eee5fab408992e9c18d7ff09ee97
Mr. Pyram <pyram3d@gmail.com>
2012-11-14 10:28:47 -0600

    Added more FastMath.java stuff.

--- src/com/jpii/navalbattle/renderer/FastMath.java
+++ src/com/jpii/navalbattle/renderer/FastMath.java
@@ -1,6 +1,12 @@
 package com.jpii.navalbattle.renderer;
 
 public class FastMath {
+	/**
+	 * Stolen from Quake. Known to the Computer Science community as the "wonder" method.
+	 * Perhaps this is the best known Math method in computer science.
+	 * @param x The number to invert square root.
+	 * @return The square inverse of the number.
+	 */
 	public static double invSqrt(double x) {
 	    double xhalf = 0.5 * x; 
 	    long i = Double.doubleToRawLongBits(x);
@@ -9,7 +15,60 @@ public class FastMath {
 	    x = x * (1.5 - xhalf*x*x); 
 	    return x; 
 	}
+	/**
+	 * Returns the square root of a number.
+	 * @param x The number to square.
+	 * @return A square root.
+	 */
 	public static double sqrt1(double x) {
 		return 1.0 / invSqrt(x);
+	}
+	/**
+	 * Trig sine function estimator.
+	 * @param x
+	 * @return
+	 */
+	public static double sin(double x) {
+		x /= (Math.PI/2);
+		double x2 = x * x;
+		return ((((.00015148419 * x2 - .00467376557) * x2 + .07968967928) * x2 - .64596371106) * x2 + 1.57079631847) * x;
+	}
+	/**
+	 * Trig cosine function estimator.
+	 * @param x
+	 * @return
+	 */
+	public static double cos(double x) {
+		return sqrt1(1 - sin(x));
+	}
+	public static double atan2(double y, double x)
+	{
+	         double absx, absy, val, M_PI, M_PI_2;
+	         M_PI = Math.PI;
+	         M_PI_2 = M_PI/2;
+	 
+	         if (x == 0 && y == 0) {
+	                 return 0;
+	         }
+	         absy = y < 0 ? -y : y;
+	         absx = x < 0 ? -x : x;
+	         if (absy - absx == absy) {
+	                 /* x negligible compared to y */
+	                 return y < 0 ? -M_PI_2 : M_PI_2;
+	         }
+	         if (absx - absy == absx) {
+	                 /* y negligible compared to x */
+	                 val = 0.0;
+	         }
+	         else    val = atan(y/x);
+	         if (x > 0) {
+	                 /* first or fourth quadrant; already correct */
+	                 return val;
+	         }
+	         if (y < 0) {
+	                 /* third quadrant */
+	                 return val - M_PI;
+	         }
+	         return val + M_PI;
 	}
 }
