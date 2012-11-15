fc54bfc7fa6ac5075fa591674d09d4d31beb9089
Mr. Pyram <pyram3d@gmail.com>
2012-11-15 12:07:20 -0600

    Added Particle Effect Tamales.

--- src/com/jpii/navalbattle/game/entity/TamaleEntity.java
+++ src/com/jpii/navalbattle/game/entity/TamaleEntity.java
@@ -13,21 +13,21 @@ public class TamaleEntity extends Entity {
 	Random r;
 	public TamaleEntity() {
 		r = new Random();
-		//system = new ParticleEngine(50, 50, 50);
-		//system.setMaxParticles(10);
+		system = new ParticleEngine(50, 50, 50);
+		system.setMaxParticles(10);
 	}
 	public void update() {
 		setImage(new BufferedImage(50,50,BufferedImage.TYPE_INT_ARGB));
 		Graphics g = getImage().getGraphics();
-		g.setColor(Helper.randomColor(r));
-		g.fillRect(0,0,50,50);
-		/*system.update();
+		//g.setColor(Helper.randomColor(r));
+		//g.fillRect(0,0,50,50);
+		system.update();
 		
 		Particle p = new FireParticle(r,(float)(r.nextDouble() * 5)+5);
 		p.setX(25);
 		p.setY(25);
 		system.addParticle(p);
 		
-		g.drawImage(system.getBuffer(),0,0,null);*/
+		g.drawImage(system.getBuffer(),0,0,null);
 	}
 }
