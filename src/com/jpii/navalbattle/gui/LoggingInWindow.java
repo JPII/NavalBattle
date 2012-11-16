6b18da53569cc4a5ee0730d0105fdba64c149d7a
TexasGamer <tpgaubert@gmail.com>
2012-11-15 22:48:11 -0600

    Removed click to skip splash screens

--- src/com/jpii/navalbattle/gui/LoggingInWindow.java
+++ src/com/jpii/navalbattle/gui/LoggingInWindow.java
@@ -1,18 +1,13 @@
 package com.jpii.navalbattle.gui;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 import javax.swing.*;
 
-import com.jpii.navalbattle.NavalBattle;
 import com.jpii.navalbattle.data.Constants;
 
 @SuppressWarnings("serial")
 public class LoggingInWindow extends Window {
 	
 	private ImageChanger imageChanger;
-	private boolean alreadyOpened = false;
 	
 	public LoggingInWindow() {
 		getContentPane().setLayout(null);
@@ -31,30 +26,6 @@ public class LoggingInWindow extends Window {
 		getContentPane().add(label);
 		
 		imageChanger.start();
-		addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				if (alreadyOpened) {
-					try {
-						dispose();
-						return;
-					} catch (Exception ex) {
-						return;
-					}
-				}
-				
-				NavalBattle.getDebugWindow().printInfo("Skipping splash screens");
-				
-				try {
-					dispose();
-				}
-				catch (Exception ex) {
-				}
-				alreadyOpened = true;
-				new MainMenuWindow();
-				
-				imageChanger.interrupt();//imageChanger.stop();
-			}
-		});
 	}
 	
 	class ImageChanger extends Thread {
