f6b79fa29b3b49f413785bf57ab796b909ff5289
Mr. Pyram <pyram3d@gmail.com>
2012-12-04 20:26:34 -0600

    Attempted to fix c6329feea8b4c76ccbfd85626c5c989de38bd2a2.

--- src/com/jpii/navalbattle/pavo/Chunk.java
+++ src/com/jpii/navalbattle/pavo/Chunk.java
@@ -41,14 +41,31 @@ public class Chunk extends Renderable {
 		Graphics g = buffer.getGraphics();
 		for (int lsx = 0; lsx < 100; lsx++) {
 			for (int lsz = 0; lsz < 100; lsz++) {
-				int opcode = (int)(Math.abs(getPoint(lsx,lsz))*127);
-				int h = 0;
-				int j = 0;
-				h = lsx % 5;
-				j = lsz % 5;
-				opcode = 110+(int)((opcode * 5) + p.noise(h, j)*95)/100;
-				g.setColor(new Color(opcode,opcode,opcode));
-				g.fillRect(lsx*3,lsz*3,3,3);
+				int opcode = (int)(Math.abs(getPoint(lsx/10,lsz/10))*127);
+				//double h = 0;
+				//double j = 0;
+				//double s = 5;
+				//double hn = 1.0/s;
+				//double cx = hn * x;
+				//double cz = hn * z;
+				//h = p.lerp(lsx/100.0, cx,cx+hn);
+				//j = p.lerp(lsx/100.0, cz,cz+hn);
+				opcode = (int)(((opcode*4.0) + (getPoint(lsx,lsz)*6.0*127)) / 10.0);
+				opcode = (int)(((opcode*2.75)+(Math.abs(getPoint(lsx/4,lsz/4)*127.0*2.25)))/10.0);
+				opcode = (int)(((opcode*6.0) + (Math.abs(getPoint(lsx/50,lsz/50)*4.0)))/10.0);
+				
+				opcode += 168;
+				//opcode = 30+(int)(Math.abs(p.noise(h, j)*127)+opcode)/2;//110+(int)((opcode * 5) + p.noise(h, j)*95)/100;
+				//g.setColor(new Color(opcode,opcode,opcode));
+				if (opcode > 255)
+					opcode = 255;
+				if (opcode < 0)
+					opcode = 0;
+				if (opcode < 200)
+					g.setColor(new Color(0,10,opcode));
+				else
+					g.setColor(new Color(10,opcode,15));
+				g.fillRect(lsx*3,lsz*3,4,4);
 			}
 		}
 		//ready = true;
