af398ecfd95997fad6ec624a1c8b0db76b47f4e6
TexasGamer <tpgaubert@gmail.com>
2012-12-07 09:37:09 -0600

    Improve Analytics API

--- src/com/roketgamer/analytics/Analytics.java
+++ src/com/roketgamer/analytics/Analytics.java
@@ -1,31 +1,29 @@
 package com.roketgamer.analytics;
 
 import com.jpii.navalbattle.NavalBattle;
-import com.jpii.navalbattle.data.Constants;
 
 public class Analytics {
 	
 	public Analytics() {
 		NavalBattle.getDebugWindow().printInfo("RoketGamer analytics initialized");
-		submit();
+		submitCore();
+		submitCustom();
 	}
 	
-	
 	@SuppressWarnings("unused")
-	public void submit() {
+	public void submitCore() {
 		// System
 		String os = System.getProperty("os.name");
 		String osVersion = System.getProperty("os.version");
 		String osArch = System.getProperty("os.arch");
 		String javaVersion = System.getProperty("java.version");
 		
-		// NavalBattle
-		String nbVersionCode = Constants.VERSION_CODE;
-		String nbVersion = Constants.NAVALBATTLE_VERSION;
-		String nbDebug = Boolean.toString(Constants.DEBUG_MODE);
-		
 		// RoketGamer
 		String rgPlayer = NavalBattle.getRoketGamer().getPlayer().getName();
 		String rgVersion = NavalBattle.getRoketGamer().getVersion();
+	}
+	
+	public void submitCustom() {
+		
 	}
 }
