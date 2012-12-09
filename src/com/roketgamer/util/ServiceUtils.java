a09c2812f98f477b696406fe0aa9747e13fdbfd5
TexasGamer <tpgaubert@gmail.com>
2012-12-08 18:41:46 -0600

    ServiceUtils

---
+++ src/com/roketgamer/util/ServiceUtils.java
@@ -0,0 +1,23 @@
+package com.roketgamer.util;
+
+public class ServiceUtils {
+	
+	/**
+	 * Returns if <code>Sessions</code> are being issued by
+	 * the server.
+	 * @return
+	 */
+	public static boolean isSessionsEnabled() {
+		// TODO: Implement
+		return false;
+	}
+	
+	/**
+	 * Returns if new account registration is enabled.
+	 * @return
+	 */
+	public static boolean isRegistrationEnabled() {
+		// TODO: Implement
+		return false;
+	}
+}
