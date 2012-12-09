900cb1d1dfeed057ebb35392ad762db3ad92a8bf
TexasGamer <tpgaubert@gmail.com>
2012-12-08 18:38:21 -0600

    Added PlayerUtils

---
+++ src/com/roketgamer/util/PlayerUtils.java
@@ -0,0 +1,86 @@
+package com.roketgamer.util;
+
+import com.roketgamer.Player;
+
+public class PlayerUtils {
+	
+	/**
+	 * Convert username to user ID.
+	 * @param username
+	 * @return
+	 */
+	public int toId(String username) {
+		// TODO: Implement
+		return 0;
+	}
+	
+	/**
+	 * Convert user ID to username.
+	 * @param id
+	 * @return
+	 */
+	public String toString(int id) {
+		// TODO: Implement
+		return "";
+	}
+	
+	/**
+	 * Get a user's number of friends.
+	 * @param username
+	 * @return
+	 */
+	public int getFriendCount(String username) {
+		// TODO: Implement
+		return 0;
+	}
+	
+	/**
+	 * Get a user's number of friends.
+	 * @param id
+	 * @return
+	 */
+	public int getFriendCount(int id) {
+		// TODO: Implement
+		return 0;
+	}
+	
+	/**
+	 * Get a user's number of friends.
+	 * @param player
+	 * @return
+	 */
+	public int getFriendCount(Player player) {
+		// TODO: Implement
+		return 0;
+	}
+	
+	/**
+	 * Get URL to a user's Gravatar (profile) image.
+	 * @param username
+	 * @return
+	 */
+	public String getGravatarUrl(String username) {
+		// TODO: Implement
+		return "";
+	}
+	
+	/**
+	 * Get URL to a user's Gravatar (profile) image.
+	 * @param id
+	 * @return
+	 */
+	public String getGravatarUrl(int id) {
+		// TODO: Implement
+		return "";
+	}
+	
+	/**
+	 * Get URL to a user's Gravatar (profile) image.
+	 * @param player
+	 * @return
+	 */
+	public String getGravatarUrl(Player player) {
+		// TODO: Implement
+		return "";
+	}
+}
