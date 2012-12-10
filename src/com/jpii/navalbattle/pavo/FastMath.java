7c5f86de707f2499c3f40a2bb88f335502c93851
Mr. Pyram <pyram3d@gmail.com>
2012-12-10 13:54:21 -0600

    Small fast math modification.

--- src/com/jpii/navalbattle/pavo/FastMath.java
+++ src/com/jpii/navalbattle/pavo/FastMath.java
@@ -30,7 +30,7 @@ public class FastMath {
 	 */
 	public static double sin(double x) {
 		//x *= (Math.PI/2);
-		double x2 = x * x;
+		double x2 = x;// * x;
 		return ((((.00015148419 * x2 - .00467376557) * x2 + .07968967928) * x2 - .64596371106) * x2 + 1.57079631847) * x;
 	}
 	/**
