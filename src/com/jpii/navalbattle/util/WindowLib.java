326376a599bbf9ad5a0adf5a3457a47cc38b0ade
Mr. Pyram <pyram3d@gmail.com>
2012-12-13 06:54:01 -0600

    Added evil hack to attempt to fix issue #86.

--- src/com/jpii/navalbattle/util/WindowLib.java
+++ src/com/jpii/navalbattle/util/WindowLib.java
@@ -2,17 +2,42 @@ package com.jpii.navalbattle.util;
 
 import java.awt.DisplayMode;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 public class WindowLib {
 	JFrame wnd;
 	int sizew, sizeh;
 	boolean ready = false;
+	Timer evilHackTimer;
 	public WindowLib(JFrame wnd) {
 		this.wnd = wnd;
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				update();
+			}
+		};
+		evilHackTimer = new Timer(75,al);
+		evilHackTimer.start();
+	}
+	private void update() {
+		if (wnd != null) {
+			if (ready) {
+				wnd.toFront();
+			}
+			else {
+				evilHackTimer.stop();
+			}
+		}
+		else {
+			evilHackTimer.stop();
+		}
 	}
 	public boolean showFullscreen() {
+		evilHackTimer.start();
 		if (FileUtils.getPlatform() == OS.windows) {
 			ready = true;
 			if (wnd == null)
@@ -44,5 +69,7 @@ public class WindowLib {
 		wnd.setSize(sizew, sizeh);
 		wnd.setAlwaysOnTop(false);
 		wnd.toFront();
+		ready = false;
+		evilHackTimer.stop();
 	}
 }
