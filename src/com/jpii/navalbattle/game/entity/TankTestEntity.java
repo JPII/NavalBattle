6a34a3b3689f6391c1ed33b42d6dc5b8c2e0ca4e
Mr. Pyram <pyram3d@gmail.com>
2012-12-29 15:45:35 -0600

    Tank moves around now.

--- src/com/jpii/navalbattle/game/entity/TankTestEntity.java
+++ src/com/jpii/navalbattle/game/entity/TankTestEntity.java
@@ -12,6 +12,17 @@ public class TankTestEntity extends Entity {
 		setId(0x93AF9B);
 	}
 	public void onMouseMove(int x, int y) {
-		getManager().moveEntity(getLocation().getRow(),getLocation().getCol(),getLocation().getRow()+1,getLocation().getCol());
+		// If the mouse is in the upper left quadrant, then move is back towards that way....etc.
+		int addx = 0;
+		int addy = 0;
+		if (x < 25)
+			addx = -1;
+		else
+			addx = 1;
+		if (y < 25)
+			addy = -1;
+		else
+			addy = 1;
+		getManager().moveEntity(getLocation().getRow(),getLocation().getCol(),getLocation().getRow()+addy,getLocation().getCol()+addx);
 	}
 }
