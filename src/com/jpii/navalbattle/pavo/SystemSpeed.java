a364ee2b3468f2ddb7bca82c9df6c4162ec562fe
Mr. Pyram <pyram3d@gmail.com>
2013-01-09 10:25:34 -0600

    Added SystemSpeed.

---
+++ src/com/jpii/navalbattle/pavo/SystemSpeed.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.jpii.navalbattle.pavo;
+
+/**
+ * @author maximusvladimir
+ * An enum describing the speed of the platform.
+ */
+public enum SystemSpeed {
+	/**
+	 * Very fast
+	 */
+	WHALE,
+	/**
+	 * Fast
+	 */
+	CHEETAH,
+	/**
+	 * Acceptable
+	 */
+	HARE,
+	/**
+	 * Slow
+	 */
+	CREEPER,
+	/**
+	 * Slowest
+	 */
+	TURTLE
+}
