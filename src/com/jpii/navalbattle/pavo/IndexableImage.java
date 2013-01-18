4282cad85754ef06d4c4e82d1360126f7ae35e54
Mr. Pyram <pyram3d@gmail.com>
2013-01-17 20:25:24 -0600

    Added ImageStore. Many images are sold here.

--- src/com/jpii/navalbattle/pavo/IndexableImage.java
+++ src/com/jpii/navalbattle/pavo/IndexableImage.java
@@ -3,10 +3,32 @@
  */
 package com.jpii.navalbattle.pavo;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 /**
  * @author maximusvladimir
  *
  */
 public class IndexableImage {
-
+	
+}
+class ImageStore {
+	ArrayList<$99u234rjie0230r9u330rj902rh308h209redh390hfr> store;
+	/**
+	 * A store that manages, sells, and buys images.
+	 */
+	public ImageStore() {
+		store = new ArrayList<$99u234rjie0230r9u330rj902rh308h209redh390hfr>();
+	}
+	public void sellImage(BufferedImage img, int asIndex) {
+		
+	}
+}
+class $99u234rjie0230r9u330rj902rh308h209redh390hfr {
+	public BufferedImage mjw9jJEWrih2;
+	public int iehgr890023jf90q;
+	public $99u234rjie0230r9u330rj902rh308h209redh390hfr() {
+		
+	}
 }
