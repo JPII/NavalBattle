d45393486a84be72dbb3b2e39d8ddf5580207d4d
Mr. Pyram <pyram3d@gmail.com>
2013-01-18 23:02:06 -0600

    Genericization

--- src/com/jpii/navalbattle/pavo/Tile.java
+++ src/com/jpii/navalbattle/pavo/Tile.java
@@ -9,16 +9,16 @@ import com.jpii.navalbattle.game.entity.Entity;
  * @author maximusvladimir
  * Shouldn't be used according to the user's 
  */
-public class Tile {
-	Entity parent;
-	int imgId = -1;
-	public Tile(Entity parent, int r, int c) {
+public class Tile<T> {
+	T parent;
+	Entity ent;
+	IndexableImage img;
+	public Tile(T parent, int r, int c) {
 		this.parent = parent;
+		if (!(parent instanceof Entity))
+			throw new RuntimeException("The provided object is not an entity.");
 	}
-	public int getImage() {
-		if (imgId == -1) {
-			throw new java.lang.RuntimeException("Image was not properly initialised. Please contact the image store for details.");
-		}
-		return imgId;
+	public T getEntity() {
+		return this.parent;
 	}
 }
