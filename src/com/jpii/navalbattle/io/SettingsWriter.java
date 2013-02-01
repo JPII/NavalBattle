bff1dc0ace475e04962f313949dbc790e1328979
Mr. Pyram <pyram3d@gmail.com>
2013-01-31 18:57:46 -0600

    Added SettingsWriter.

---
+++ src/com/jpii/navalbattle/io/SettingsWriter.java
@@ -0,0 +1,70 @@
+package com.jpii.navalbattle.io;
+
+import java.io.*;
+import java.util.*;
+
+public class SettingsWriter {
+	
+	ArrayList<SettingsAttribute> attributes;
+	String path;
+	
+	/**
+	 * <code>SettingsReader</code> constructor.
+	 * @param filePath		Location to store the file
+	 * @param attributes	Attributes to save
+	 */
+	public SettingsWriter(String filePath, ArrayList<SettingsAttribute> attributes) {
+		path = filePath;
+		this.attributes = attributes;
+		if (attributes == null || attributes.size() <= 0) {
+			throw new java.lang.IllegalArgumentException("The settings that you would like to read is not filled.");
+		}
+	}
+	
+	/**
+	 * Read settings file.
+	 */
+	public void read() {
+		try {
+			PrintWriter pw = new PrintWriter(new FileWriter(path));
+			for (int c = 0; c < attributes.size(); c++) {
+				SettingsAttribute s = attributes.get(c);
+				if (s != null && s.name != null && s.value != null)
+					pw.println(s.name + ":" + s.value);
+				else if (s != null && s.name != null)
+					pw.println(s.name + ":");
+			}
+
+			pw.close();
+		}
+		catch (Exception ex) {
+			System.err.println(ex.getMessage());
+		}
+	}
+	
+	/**
+	 * Find attribute location.
+	 * @param str
+	 * @return
+	 */
+	private int findLineAttributeLocation(String str) {
+		for (int c = 0; c < attributes.size(); c++) {
+			SettingsAttribute attr = attributes.get(c);
+			if (str.toLowerCase().equals(attr.getName().toLowerCase()))
+				return c;
+		}
+		return -1;
+	}
+	
+	/**
+	 * Returns data read from the settings file.
+	 * @return
+	 */
+	public ArrayList<SettingsAttribute> getData() {
+		return attributes;
+	}
+	
+	public void setData(ArrayList<SettingsAttribute> data) {
+		attributes = data;
+	}
+}
