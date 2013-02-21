5de1a15ab700cb17b25077badfa216cf95f9aede
Mr. Pyram <pyram3d@gmail.com>
2013-02-21 08:24:50 -0600

    Probably finished PProgress.

--- src/com/jpii/navalbattle/pavo/gui/controls/PProgress.java
+++ src/com/jpii/navalbattle/pavo/gui/controls/PProgress.java
@@ -3,6 +3,10 @@
  */
 package com.jpii.navalbattle.pavo.gui.controls;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+
 /**
  * @author maximusvladimir
  * Progress bar class.
@@ -37,5 +41,21 @@ public class PProgress extends Control {
 	
 	public float getProgress(float outOf) {
 		return progress * outOf;
+	}
+	
+	public void paint(Graphics2D g) {
+		if (getHeight() > 5) {
+			g.setColor(getBackgroundColor().darker());
+			g.fillRect(0,0,getWidth(),getHeight());
+			GradientPaint gradient = new GradientPaint(0,0,new Color(95,166,74),0,getHeight(),new Color(63,107,68));
+			g.setPaint(gradient);
+			g.fillRect(2,2,(int)(progress*(getWidth()-4)),getHeight()-4);
+		}
+		else {
+			GradientPaint gradient = new GradientPaint(0,0,new Color(95,166,74),0,getHeight(),new Color(63,107,68));
+			g.setPaint(gradient);
+			g.fillRect(0,0,(int)(progress*(getWidth()-4)),getHeight());
+		}
+			
 	}
 }
