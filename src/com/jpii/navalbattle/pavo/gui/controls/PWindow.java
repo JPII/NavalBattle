94bc9f08183fe049e398fb5f8cf850cc170da742
Mr. Pyram <pyram3d@gmail.com>
2013-02-21 08:19:13 -0600

    Added PWindow.

---
+++ src/com/jpii/navalbattle/pavo/gui/controls/PWindow.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.jpii.navalbattle.pavo.gui.controls;
+
+/**
+ * @author maximusvladimir
+ *
+ */
+public class PWindow extends Control {
+
+	/**
+	 * @param parent
+	 */
+	public PWindow(Control parent) {
+		super(parent);
+	}
+	
+	public void paint(Graphics2D g) {
+		
+	}
+}
