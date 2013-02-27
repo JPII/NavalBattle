2074abf5c171a0ee751a45c29f93db7f2976e319
abauer <abauer@jpiicardinals.org>
2013-02-27 13:45:14 -0600

    cleaned up HUD

--- src/com/jpii/navalbattle/game/gui/HUD.java
+++ src/com/jpii/navalbattle/game/gui/HUD.java
@@ -53,30 +53,11 @@ public class HUD extends PWindow{
 		repaint();
 	}
 	
-	private void drawMenu(){
-		
-	}
-	
-	private Color getBlue(){
-		int r = ran.nextInt(85,115);
-		int g = ran.nextInt(0,255/2);
-		int b = ran.nextInt((255/2)+g/2,255);
-		System.out.println("Blue: "+new Color(r,g,b).toString());
-		return new Color(r,g,b);
-	}
-	
 	private Color getBlack(){
 		int r = 0;
 		int b = ran.nextInt(50,(255/3));
 		System.out.println("Black: "+new Color(r,r,b).toString());
 		return new Color(r,r,b);
-	}
-	
-	private Color getBrown(){
-		int r = ran.nextInt(85,115);
-		int g = ran.nextInt(0,255/2);
-		int b = ran.nextInt(0,255/3);
-		return new Color(r,g,b);
 	}
 	
 }
