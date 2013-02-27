3fad018b390574bd72963c53940f54018e66a3e1
maximusvladimir <pyram3d@gmail.com>
2013-02-26 20:30:32 -0600

    Equilibrium.

--- src/com/jpii/navalbattle/game/gui/HUD.java
+++ src/com/jpii/navalbattle/game/gui/HUD.java
@@ -21,17 +21,18 @@ public class HUD extends PWindow{
 		super(parent, x, y, width, height);
 		display = null;
 		ran = Game.Settings.rand;
-<<<<<<< HEAD
-		gp = new GradientPaint(0,0,new Color(0,0,255),0,height,new Color(0,0,0));
-=======
+		
 		gp = new GradientPaint(0,0,getBlue(),0,height,getBlack());
+		
+		
 	}
 	public HUD(WindowManager parent){
 		super(parent);
 		display = null;
 		ran = Game.Settings.rand;
+		
 		gp = new GradientPaint(0,0,getBlue(),0,height,getBlack());
->>>>>>> Fixed Anthony.
+		
 	}
 	
 	public void paint(Graphics2D g) {
