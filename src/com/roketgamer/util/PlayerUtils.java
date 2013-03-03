06c4c45c048b730bd2a141154ed382c8fd564ff1
TexasGamer <tpgaubert@gmail.com>
2013-03-03 13:40:32 -0600

    Remove Gravatar methods from PlayerUtils

--- src/com/roketgamer/util/PlayerUtils.java
+++ src/com/roketgamer/util/PlayerUtils.java
@@ -100,34 +100,4 @@ public class PlayerUtils {
 		// TODO: Implement
 		return 0;
 	}
-	
-	/**
-	 * Get URL to a user's Gravatar (profile) image.
-	 * @param username
-	 * @return
-	 */
-	public static String getGravatarUrl(String username) {
-		// TODO: Implement
-		return "";
-	}
-	
-	/**
-	 * Get URL to a user's Gravatar (profile) image.
-	 * @param id
-	 * @return
-	 */
-	public static String getGravatarUrl(int id) {
-		// TODO: Implement
-		return "";
-	}
-	
-	/**
-	 * Get URL to a user's Gravatar (profile) image.
-	 * @param player
-	 * @return
-	 */
-	public static String getGravatarUrl(Player player) {
-		// TODO: Implement
-		return "";
-	}
 }
