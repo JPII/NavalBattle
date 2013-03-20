ec301c47e14de6f7c3b08abd768432cce069c9b9
TexasGamer <tpgaubert@gmail.com>
2013-03-20 13:27:08 -0500

    stuff

--- src/com/jpii/navalbattle/gui/UpdateWindow.java
+++ src/com/jpii/navalbattle/gui/UpdateWindow.java
@@ -1,5 +1,65 @@
 package com.jpii.navalbattle.gui;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+
+import com.jpii.navalbattle.NavalBattle;
+import com.jpii.navalbattle.util.URLUtils;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UpdateWindow extends Window {
-
+	
+	JLabel announcementTitle;
+	JTextPane announcementText;
+	
+	public UpdateWindow() {
+		getContentPane().setLayout(null);
+		
+		JLabel lblAnnouncement = new JLabel("Announcement:");
+		lblAnnouncement.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblAnnouncement.setBounds(10, 11, 128, 30);
+		getContentPane().add(lblAnnouncement);
+		
+		announcementTitle = new JLabel("Announcement title");
+		announcementTitle.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		announcementTitle.setBounds(143, 19, 333, 14);
+		getContentPane().add(announcementTitle);
+		
+		announcementText = new JTextPane();
+		announcementText.setText("Announcement text");
+		announcementText.setEditable(false);
+		announcementText.setBounds(10, 44, 466, 221);
+		getContentPane().add(announcementText);
+		
+		JButton btnClose = new JButton("Close");
+		btnClose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				nextWindow("LoginWindow");
+			}
+		});
+		btnClose.setBounds(387, 278, 89, 23);
+		getContentPane().add(btnClose);
+		
+		JButton moreInfoButton = new JButton("More Info");
+		moreInfoButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				URLUtils.openURL(NavalBattle.getBroadcastService().getAnnouncementUrl());
+			}
+		});
+		moreInfoButton.setBounds(10, 278, 89, 23);
+		getContentPane().add(moreInfoButton);
+	}
+	
+	public void setVisible(boolean status) {
+		super.setVisible(status);
+		
+		if(status) {
+			announcementTitle.setText(NavalBattle.getBroadcastService().getAnnouncementTitle());
+			announcementText.setText(NavalBattle.getBroadcastService().getAnnouncementText());
+		}
+	}
 }
