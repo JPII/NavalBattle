e7c4d031c005149762928f3e04ffcdc9d2815ce2
TexasGamer <tpgaubert@gmail.com>
2013-03-24 21:42:15 -0500

    Bump version!

--- src/com/jpii/navalbattle/data/Constants.java
+++ src/com/jpii/navalbattle/data/Constants.java
@@ -26,8 +26,8 @@ public class Constants {
 	/*
 	 * General
 	 */
-	public static final String NAVALBATTLE_VERSION = "0.5a";
-	public static final String VERSION_CODE = "5";
+	public static final String NAVALBATTLE_VERSION = "0.6b";
+	public static final String VERSION_CODE = "6";
 	public static final String NAVALBATTLE_CODENAME = "Avenger";
 	public static final String NAVALBATTLE_VERSION_TITLE = "NavalBattle " + NAVALBATTLE_VERSION + " (" + NAVALBATTLE_CODENAME + ")";
 	public static final String NAVALBATTLE_UPDATE_URL = "https://raw.github.com/JPII/NavalBattle/master/update.xml";
