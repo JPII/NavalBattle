2f9c8dc097dc580f64ebfd05a1a15aedab326fb9
Smeagle42 <zmathewson@jpiicardinals.org>
2013-03-27 21:18:48 -0500

    I don't even know anymore

--- src/com/jpii/navalbattle/game/gui/History.java
+++ src/com/jpii/navalbattle/game/gui/History.java
@@ -1,30 +1,18 @@
 package com.jpii.navalbattle.game.gui;
 
-import java.util.*;
-
 public class History {
-
-	private ArrayList<String> history;
-	private int moves;
-
+	
+	String[] moves;
+	
 	public History(){
-		history = new ArrayList<String>();
-		moves = 0;
+		
 	}
-
-	public boolean isEmpty(){
-		return history.size() == 0;
-	}
-
+	
 	public void add(String x){
-		history.add(x);
-	}
 		
-	public String remove(){
-		return history.remove(moves);
 	}
+	
+	public void remove(int y){
 		
-	public String peek(){
-		return history.get(moves);
 	}
 }
