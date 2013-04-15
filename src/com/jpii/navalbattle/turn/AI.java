a1cf0f0023acce33ad5a2b4dc740541e7598c6c5
DarkWarHero <jrthelettuce@gmail.com>
2013-04-15 13:59:15 -0500

    Hi matt
    Work in progress

--- src/com/jpii/navalbattle/turn/AI.java
+++ src/com/jpii/navalbattle/turn/AI.java
@@ -1,22 +1,39 @@
 package com.jpii.navalbattle.turn;
 
+import java.util.ArrayList;
+
 import com.jpii.navalbattle.game.NavalManager;
+import com.jpii.navalbattle.game.entity.MoveableEntity;
+import com.jpii.navalbattle.pavo.grid.Entity;
+import com.jpii.navalbattle.pavo.grid.EntityManager;
+import com.jpii.navalbattle.pavo.grid.Location;
 
 public class AI extends Player{
 	
 	NavalManager nm;
+	ArrayList<Entity> enemies;
 	
 	public AI(NavalManager nm,String name) {
 		super(name);
+		enemies = new ArrayList<Entity>();
 		this.nm = nm;
 	}
 	
+	public void addEntity(Entity e){
+		enemies.add(e);
+	}
+	
+	
 	public void takeTurn(){
+		for(int k = 0; k < getTotalEntities(); k++)
+		{
+			Entity ent = getEntity(k);
+			MoveableEntity currentEntity = (MoveableEntity)ent;
+		}
 		turnOver=true;
 	}
 	
 	public void endTurn(){
 		super.endTurn();
 	}
-	
 }
