415b6ff477529102a63b05a1d862610fc39e0b7c
abauer <abauer@jpiicardinals.org>
2013-04-16 14:06:37 -0500

    fixed Portchops and chipshots

--- src/com/jpii/navalbattle/turn/ShipShop.java
+++ src/com/jpii/navalbattle/turn/ShipShop.java
@@ -10,77 +10,83 @@ import com.jpii.navalbattle.pavo.gui.controls.PWindow;
 
 	public class ShipShop extends PWindow {
 
-	 public ShipShop(NewWindowManager pare){
-		 super(pare);
-		 setVisible(true);
-		 pare.add(this);
-		 setLoc(78, 86);
-		 setSize(500, 250);
-		 setText("Ship Shop");
+		public ShipShop(NewWindowManager parent){
+			super(parent);
+			setVisible(true);
+			parent.add(this);
+			setSize(500, 250);
+			setLoc(390, 275);
+			setText("Ship Shop");
+			initItems();
+		}
+	 
+		private void initItems(){
+			PText shop = new PText(this);
+			PText score = new PText(this);
+			PButton hull = new PButton(this);
+			PButton missile = new PButton(this);
+			PButton range = new PButton(this);
+			PButton antimissile = new PButton(this);
+			PButton repair = new PButton(this);
 		 
-		 PText text0 = new PText(this);
-		 text0.setText("Ship Shop");
-		 text0.setLoc(199, 32);
-		 text0.setFont(new Font("Verdana Bold", 1, 18));
-		 addControl(text0);
+			PText stock = new PText(this);
+			PText price = new PText(this);
+			PText hprice = new PText(this);
+			PText mprice = new PText(this);
+			PText rprice = new PText(this);
+			PText amprice = new PText(this);
+			PText reprice = new PText(this);
 		 
-		 PText text1 = new PText(this);
-		 text1.setText("Your Current Score is "+NavalGame.getManager().getTurnManager().getTurn().getPlayer().getScore());
-		 text1.setLoc(4, 224);
-		 addControl(text1);
+			shop.setFont(new Font("Verdana Bold", 1, 18));
+			score.setFont(new Font("Verdana Bold", 1, 12));
+			stock.setFont(new Font("Verdana Bold", 1, 12));
+			price.setFont(new Font("Verdana Bold", 1, 12));
 		 
-		 PText text2 = new PText(this);
-		 text2.setText("Stock");
-		 text2.setLoc(55, 60);
-		 addControl(text2);
-		 PText text3 = new PText(this);
-		 text3.setText("Price");
-		 text3.setLoc(388, 63);
-		 addControl(text3);
+			shop.setText("Ship Shop");
+			score.setText("Your Current Score is "+NavalGame.getManager().getTurnManager().getTurn().getPlayer().getScore());
+			hull.setText("Hull Upgrade");
+			missile.setText("Missile X5");
+			range.setText("Increase Range");
+		 	antimissile.setText("Anti-Missile");
+		 	repair.setText("Repair Ship");
 		 
-		 PButton button1 = new PButton(this);
-		 button1.setText("Repair Ships");
-		 button1.setLoc(16, 86);
-		 addControl(button1);
-		 PText text4 = new PText(this);
-		 text4.setText("300");
-		 text4.setLoc(390, 86);
-		 addControl(text4);
+		 	stock.setText("Stock");
+		 	price.setText("Price");
+		 	hprice.setText("200");
+		 	mprice.setText("250");
+		 	rprice.setText("700");
+		 	amprice.setText("350");
+		 	reprice.setText("300");
 		 
-		 PButton button2 = new PButton(this);
-		 button2.setText("Missile X5");
-		 button2.setLoc(16, 114);
-		 addControl(button2);
-		 PText text5 = new PText(this);
-		 text5.setText("250");
-		 text5.setLoc(390, 114);
-		 addControl(text5);
+		 	shop.setLoc(200, 35);
+		 	score.setLoc(5, 230);
+			hull.setLoc(20, 85);
+			missile.setLoc(20, 115);
+			range.setLoc(20, 145);
+			antimissile.setLoc(20, 175);
+			repair.setLoc(20, 205);
 		 
-		 PButton button3 = new PButton(this);
-	  	 button3.setText("Increase Range: 0 of 3");
-	  	 button3.setLoc(16, 143);
-	  	 addControl(button3);
-	  	 PText text6 = new PText(this);
-		 text6.setText("700");
-		 text6.setLoc(390, 143);
-		 addControl(text6);
+			stock.setLoc(60, 60);
+			price.setLoc(390, 60);
+			hprice.setLoc(390, 85);
+			mprice.setLoc(390, 115);
+		 	rprice.setLoc(390, 145);
+		 	amprice.setLoc(390, 175);
+		 	reprice.setLoc(390, 205);
 		 
-	  	 PButton button4 = new PButton(this);
-	  	 button4.setText("Anti-Missile");
-	  	 button4.setLoc(16, 171);
-	  	 addControl(button4);
-	  	 PText text7 = new PText(this);
-		 text7.setText("350");
-		 text7.setLoc(390, 171);
-		 addControl(text7);
-	  	 
-	  	 PButton button5 = new PButton(this);
-	  	 button5.setText("Hull Upgrade");
-	  	 button5.setLoc(16,199);
-	  	 addControl(button5);
-	  	 PText text8 = new PText(this);
-		 text8.setText("200");
-		 text8.setLoc(390,199);
-		 addControl(text8);
-	 }
+		 	addControl(shop);
+			addControl(score);
+			addControl(hull);
+			addControl(missile);
+			addControl(range);
+			addControl(antimissile);
+			addControl(repair);
+			addControl(price);
+			addControl(stock);		 
+			addControl(hprice);
+			addControl(mprice);
+			addControl(rprice);
+			addControl(amprice);
+			addControl(reprice);
+		}
 	}
