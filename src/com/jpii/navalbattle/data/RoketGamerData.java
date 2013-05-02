820e24714d28a50b40ac6dfe514819e75557e788
TexasGamer <tpgaubert@gmail.com>
2013-05-02 18:56:11 -0500

    Added final achievements

--- src/com/jpii/navalbattle/data/RoketGamerData.java
+++ src/com/jpii/navalbattle/data/RoketGamerData.java
@@ -7,21 +7,23 @@ public class RoketGamerData {
 
 	/* Achievement Objects */
 	public static final Achievement ACHIEVEMENT_PORKCHOP = new Achievement(1, "Porkchop", false);
-	public static final Achievement ACHIEVEMENT_THRIFT_SHOP = new Achievement(1, "Thrift Shop", false);
-	public static final Achievement ACHIEVEMENT_SUNK_MY_BATTLESHIP = new Achievement(1, "You Sunk My Battleship", false);
-	public static final Achievement ACHIEVEMENT_AIR_SUPERIORITY = new Achievement(1, "Air Superiority", false);
-	public static final Achievement ACHIEVEMENT_SYNCING_SHIP = new Achievement(1, "Syncing Ship", false);
-	public static final Achievement ACHIEVEMENT_BLANK_CHECK = new Achievement(1, "Blank Check", false);
-	public static final Achievement ACHIEVEMENT_PROMOTION = new Achievement(1, "Promotion", false);
-	public static final Achievement ACHIEVEMENT_WAR_BONDS = new Achievement(1, "War Bonds", false);
-	public static final Achievement ACHIEVEMENT_SHOCK_AND_AWE = new Achievement(1, "Shock and Awe", false);
-	public static final Achievement ACHIEVEMENT_LUCKY_SHOT = new Achievement(1, "Lucky Shot", false);
-	public static final Achievement ACHIEVEMENT_IT_ALL_CHANGED = new Achievement(1, "It all changed...", true);
-	public static final Achievement ACHIEVEMENT_WHEN_FIRE_NATION_ATTACKED = new Achievement(1, "...when the fire nation attacked", true);
-	public static final Achievement ACHIEVEMENT_DAVE = new Achievement(1, "Dave", true);
-	public static final Achievement ACHIEVEMENT_NAVAL_SUPERIORITY = new Achievement(1, "Naval Superiority", true);
-	public static final Achievement ACHIEVEMENT_UNSTOPPABLE = new Achievement(1, "Unstoppable", true);
-	public static final Achievement ACHIEVEMENT_COMMANDER = new Achievement(1, "Commander", true);
+	public static final Achievement ACHIEVEMENT_THRIFT_SHOP = new Achievement(2, "Thrift Shop", false);
+	public static final Achievement ACHIEVEMENT_SUNK_MY_BATTLESHIP = new Achievement(3, "You Sunk My Battleship", false);
+	public static final Achievement ACHIEVEMENT_AIR_SUPERIORITY = new Achievement(4, "Air Superiority", false);
+	public static final Achievement ACHIEVEMENT_SYNCING_SHIP = new Achievement(5, "Syncing Ship", false);
+	public static final Achievement ACHIEVEMENT_BLANK_CHECK = new Achievement(6, "Blank Check", false);
+	public static final Achievement ACHIEVEMENT_PROMOTION = new Achievement(7, "Promotion", false);
+	public static final Achievement ACHIEVEMENT_WAR_BONDS = new Achievement(8, "War Bonds", false);
+	public static final Achievement ACHIEVEMENT_SHOCK_AND_AWE = new Achievement(9, "Shock and Awe", false);
+	public static final Achievement ACHIEVEMENT_LUCKY_SHOT = new Achievement(10, "Lucky Shot", false);
+	public static final Achievement ACHIEVEMENT_IT_ALL_CHANGED = new Achievement(11, "It all changed...", true);
+	public static final Achievement ACHIEVEMENT_WHEN_FIRE_NATION_ATTACKED = new Achievement(12, "...when the fire nation attacked", true);
+	public static final Achievement ACHIEVEMENT_ANOTHER_DAY = new Achievement(13, "Another Day", true);
+	public static final Achievement ACHIEVEMENT_FALLING_FROM_TREES = new Achievement(14, "Falling From Trees", true);
+	public static final Achievement ACHIEVEMENT_DAVE = new Achievement(15, "Dave", true);
+	public static final Achievement ACHIEVEMENT_NAVAL_SUPERIORITY = new Achievement(16, "Naval Superiority", true);
+	public static final Achievement ACHIEVEMENT_UNSTOPPABLE = new Achievement(17, "Unstoppable", true);
+	public static final Achievement ACHIEVEMENT_COMMANDER = new Achievement(18, "Commander", true);
 
 	/* Leaderboard Object */
 	public static final Leaderboard LEADERBOARD_TOTAL_SCORE = new Leaderboard(1);
